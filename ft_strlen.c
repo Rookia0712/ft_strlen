81511a38b59979d9f1be991c081212bfe3b35dbe
John Richards <jorichar@F2N009.42adel.org.au>
2025-01-30 19:26:06 +1030

    String length fuction

---
+++ ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorichar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 17:56:00 by jorichar          #+#    #+#             */
+/*   Updated: 2025/01/30 19:01:46 by jorichar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/*
+int	main(void)
+{
+	char	name[] = "Ricardo";
+	int	length = ft_strlen(name);
+	printf("%d\n", length);
+	return(0);
+}
+*/
